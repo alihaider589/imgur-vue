958180033de14735eefbe87e479f171b43636d05
Ali Haider <alihaider589@outlook.com>
2021-01-22 18:34:16 +0500

    adding changes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import store from './store'
+import VueRouter from 'vue-router'
+import AuthHandler from './views/AuthHandler'
 
-Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [{ path: '/oauth2/callback', component: AuthHandler }]
+})
 new Vue({
-  render: h => h(App),
+    router: router,
+    store: store,
+    render: h => h(App)
 }).$mount('#app')
+
+
+
+
+
+
+
+// client Id:fcb2b69744ef311
+
+// client Secret :047920e7d5b1314aeae8af0dbae2a4a9a0b3f40c
